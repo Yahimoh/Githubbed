864e820a0b272e8f921d381769104da7fe098491
Yahia <yahimoh@icloud.com>
2024-01-11 15:51:37 +0300

    Initial modal view of users followed persons info, more to come fosho 💯

---
+++ Githubbed/Screens/Components/Labels/GBDSecondaryTitleLabel.swift
@@ -0,0 +1,33 @@
+//
+//  GBDSecondaryTitleLabel.swift
+//  Githubbed
+//
+//  Created by Mohamad Yahia on 11.1.2024.
+//
+
+import UIKit
+
+class GBDSecondaryTitleLabel: UILabel {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(fontSize: CGFloat) {
+        super.init(frame: .zero)
+        font = UIFont.systemFont(ofSize: fontSize, weight: .medium)
+        configure()
+    }
+    
+    private func configure() {
+        textColor = .secondaryLabel
+        adjustsFontSizeToFitWidth = true
+        minimumScaleFactor = 0.9
+        lineBreakMode = .byTruncatingTail
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
